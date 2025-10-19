73df8ddd075880ae4fb9ca1bd1c7b6260751acff
Erol Saroğlu <saroglu.erol@gmail.com>
2025-10-19 18:18:49 +0300

    api and api test projects added, sqlite pck installed and program.cs configured

---
+++ RoomBookingApp.Api.Tests/TestTheWeatherForecast.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Logging;
+using Moq;
+using RoomBookingApp.Api.Controllers;
+using Shouldly;
+using Xunit;
+
+namespace RoomBookingApp.Api.Tests
+{
+
+    public class TestTheWeatherForecast
+    {
+        [Fact]
+        public void Should_Return_Value()
+        {
+            // Arrange
+            var loggerMock = new Mock<ILogger<WeatherForecastController>>();
+            var controller = new WeatherForecastController(loggerMock.Object);
+
+            // Act
+            var result = controller.Get();
+
+            // Assert
+            result.ShouldNotBeNull();
+            result.Count().ShouldBeGreaterThan(1);
+        }
+    }
+}
---
+++ RoomBookingApp.Api/Program.cs
@@ -0,0 +1,37 @@
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using RoomBookingApp.Persistence;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var cs = builder.Configuration.GetConnectionString("DefaultConnection");
+var con = new SqliteConnection(cs);
+con.Open();
+
+builder.Services.AddDbContext<RoomBookingAppDbContext>(options =>
+    options.UseSqlite(con)
+);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
